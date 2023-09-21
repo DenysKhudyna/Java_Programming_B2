4ded2d02835589fe93a8f315cad11dd90834c3ea
dhudy <dhudyna@gmail.com>
2023-09-20 21:12:34 -0500

    My  Day10 Commit

--- src/day10_if_statements/Overtime.java
+++ src/day10_if_statements/Overtime.java
@@ -37,7 +37,7 @@ public class Overtime {
             System.out.print(message)
 
          */
-        if (numHours > 40) {
+        if (numHours > 41) {
             overtimeHours = numHours - 40;
             netPay = (hourlyRate * 40)  + (overtimeHours * hourlyRate * 1.5);
             message = "You worked "  + numHours + " hours at a rate of $" + hourlyRate + ", but " + overtimeHours + " of the hours were overtime, so you got an additional pay for those hours. Your net pay is $" + netPay;
