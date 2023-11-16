93a884e8f348f3abd1b477c122c2d2761593fa52
dhudy <dhudyna@gmail.com>
2023-11-15 21:04:58 -0600

    My  Day34 Commit "OOP Intro"

---
+++ src/day34_b_encapsulation/traffic/Road.java
@@ -0,0 +1,69 @@
+package day34_b_encapsulation.traffic;
+
+public class Road { public static void main(String[] args) {
+
+    TrafficLight light = new TrafficLight("green");
+    //light.color = "red";
+    // Since my instance variable is PRIVATE, I do NOT have direct access to it out-side of the class.
+
+
+    // How can I give an INDIRECT ACCESS to READ
+    System.out.println(light.getColor());
+
+
+    TrafficLight light2 = new TrafficLight("yellow");
+    System.out.println(light2.getColor());
+
+
+    System.out.println();
+    // Can you change and reassign the color for the objects
+    light.setColor("red");
+    light2.setColor("pink"); // Since we have some condition in setColor() method, this will not get assigned to color instance variable fo light object
+    System.out.println();
+    System.out.println(light.getColor());
+    System.out.println(light2.getColor());
+
+
+
+
+
+
+
+
+}
+
+    public static class TrafficLight {
+
+
+        private String color;
+
+        public TrafficLight (String color) {
+            this.color = color;
+        }
+
+        // READ ONLY / view only
+        public String getColor () {
+            return color;
+        }
+
+
+        public void setColor (String color) {
+
+    //        switch (color) {
+    //            case  "red":
+    //            case "yellow" :
+    //            case "green" :
+    //                    this.color = color;
+    //        }
+
+            if (color.equals("red") || color.equals("yellow") || color.equals("green")){
+                this.color = color;
+            } else {
+                System.out.println("It is not valid color for traffic light");
+            }
+
+        }
+
+
+    }
+}
